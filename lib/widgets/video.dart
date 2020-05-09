cf934f8dad357f135a1f3d8dc295be82cbab06da
HarryZhang <1056373864@qq.com>
2020-05-09 19:02:58 +0800

    考虑fijkplayer来实现播放器

---
+++ lib/widgets/video.dart
@@ -0,0 +1,66 @@
+import 'package:alltv/widgets/alltv_panel.dart';
+import 'package:fijkplayer/fijkplayer.dart';
+import 'package:flutter/material.dart';
+
+// import 'custom_ui.dart';
+
+class VideoScreen extends StatefulWidget {
+  final String url;
+
+  VideoScreen({@required this.url});
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  final FijkPlayer player = FijkPlayer();
+
+  _VideoScreenState();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setOption(FijkOption.hostCategory, "enable-snapshot", 1);
+    player.setOption(FijkOption.playerCategory, "mediacodec-all-videos", 1);
+    startPlay();
+  }
+
+  void startPlay() async {
+    await player.setOption(FijkOption.hostCategory, "request-screen-on", 1);
+    await player.setOption(FijkOption.hostCategory, "request-audio-focus", 1);
+    await player.setDataSource(widget.url, autoPlay: true).catchError((e) {
+      print("setDataSource error: $e");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: FijkView(
+            player: player,
+            color: Colors.black,
+            panelBuilder: allTVPanelBuilder(snapShot: true),
+            // panelBuilder: simplestUI,
+            // panelBuilder: (FijkPlayer player, BuildContext context,
+            //     Size viewSize, Rect texturePos) {
+            //   return CustomFijkPanel(
+            //       player: player,
+            //       buildContext: context,
+            //       viewSize: viewSize,
+            //       texturePos: texturePos);
+            // },
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
+  }
+}
